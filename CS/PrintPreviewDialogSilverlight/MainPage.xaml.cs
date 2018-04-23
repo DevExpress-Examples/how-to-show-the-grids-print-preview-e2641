842fd66d74c0845af8a23c0c644ec0792993d4ff
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PrintPreviewDialogSilverlight/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Printing;
+using DevExpress.Xpf.Printing.DataNodes;
+using DevExpress.Xpf.Grid;
+using DevExpress.Utils;
+
+namespace PrintPreviewDialogSilverlight {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = new ProductList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            ShowPrintPreview(grid);
+        }
+
+        private void ShowPrintPreview(GridControl grid) {
+            DocumentPreviewWindow preview = new DocumentPreviewWindow();
+            PrintableControlLink link = new PrintableControlLink(grid.View as DevExpress.Xpf.Printing.IPrintableControl);
+            link.ExportServiceUri = "../ExportService1.svc";
+            LinkPreviewModel model = new LinkPreviewModel(link);
+            preview.Model = model;
+            link.CreateDocument(true);
+            preview.ShowDialog();
+        }
+    }
+}
---
+++ VB/PrintPreviewDialogSilverlight/MainPage.xaml.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Printing
+Imports DevExpress.Xpf.Printing.DataNodes
+Imports DevExpress.Xpf.Grid
+Imports DevExpress.Utils
+
+Namespace PrintPreviewDialogSilverlight
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = New ProductList()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			ShowPrintPreview(grid)
+		End Sub
+
+		Private Sub ShowPrintPreview(ByVal grid As GridControl)
+			Dim preview As New DocumentPreviewWindow()
+			Dim link As New PrintableControlLink(TryCast(grid.View, DevExpress.Xpf.Printing.IPrintableControl))
+			link.ExportServiceUri = "../ExportService1.svc"
+			Dim model As New LinkPreviewModel(link)
+			preview.Model = model
+			link.CreateDocument(True)
+			preview.ShowDialog()
+		End Sub
+	End Class
+End Namespace
