be0fdb956eb4f0a704b9e6d3632bff103169c4de
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/E2641_VB.Web/ExportService1.svc.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Runtime.Serialization
+Imports System.ServiceModel
+Imports System.ServiceModel.Activation
+Imports DevExpress.Data.Utils.ServiceModel
+
+Namespace E2641_VB.Web
+	<SilverlightFaultBehavior> _
+	Public Class ExportService1
+		Inherits DevExpress.Xpf.Printing.Service.ExportService
+	End Class
+End Namespace
